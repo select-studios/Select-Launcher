57c69d81480e80da08c9b060ec3b4a72928e2dae
Game Glide <sinha.soham@outlook.com>
2022-11-29 23:19:12 +0530

    Fixed the move error finally

--- client/lib/src/api/login_api.dart
+++ client/lib/src/api/login_api.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:select_launcher/src/util/url_controller.dart';
-import 'package:select_launcher/src/util/settings_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void login(String username, String password) async {
+  final prefs = await SharedPreferences.getInstance();
+
   try {
     var response = await Dio().post(
       URLController.login,
@@ -11,14 +13,29 @@ void login(String username, String password) async {
     );
 
     if (response.statusCode == 201) {
-      SettingsManager.setAccessToken(response.data['user']['accessToken']);
-      SettingsManager.setRefreshToken(response.data['user']['refreshToken']);
-      SettingsManager.setUsername(response.data['user']['username']);
+      prefs.setString(
+        'accessToken',
+        response.data['user']['accessToken'].toString(),
+      );
+      prefs.setString(
+        'refreshToken',
+        response.data['user']['refreshToken'].toString(),
+      );
+      prefs.setString(
+        'username',
+        response.data['user']['username'].toString(),
+      );
 
       if (kDebugMode) {
-        print(SettingsManager.settings['accessToken']);
-        print(SettingsManager.settings['refreshToken']);
-        print(SettingsManager.settings['username']);
+        print(
+          <Object>[
+            {
+              prefs.getString('accessToken'),
+              prefs.getString('refreshToken'),
+              prefs.getString('username')
+            }
+          ],
+        );
       }
     }
   } catch (error, stackTrace) {
