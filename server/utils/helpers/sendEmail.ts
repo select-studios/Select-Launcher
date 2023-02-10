6a3262f99330c691bb4a0c8905dcf821de155fec
Game Glide <sinha.soham@outlook.com>
2023-02-10 22:05:34 +0530

    fix server side
    yes im just that good.

--- server/utils/helpers/sendEmail.ts
+++ server/utils/helpers/sendEmail.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+import * as nodemailer from "nodemailer";
 import { VerifyEmail } from "../../data/emails/verify/verify";
 import { render } from "@react-email/render";
 
