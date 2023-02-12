2fec301a65947a5ff48bdc8363694102365f5a35
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2023-02-12 18:15:42 +0000

    Updated games for MSTORE ss

--- server/utils/helpers/updateGamesInfo.ts
+++ server/utils/helpers/updateGamesInfo.ts
@@ -7,7 +7,7 @@ const gamesData = [
     description:
       "Fast pace action experience. An experience that is hand-picked to be enjoyed by players. Movement designed to immerse you in the experience of AceRace. Your objective is to reach the end of the game without dying once! Doing so will give you a surprise level!",
     image: {
-      icon: "https://i.imgur.com/wcij8Zf.png",
+      icon: "https://i.imgur.com/UWQr98m.png",
     },
     tags: ["action", "momentum", "fast"],
     verified: true,
@@ -16,22 +16,22 @@ const gamesData = [
     name: "Rosehill",
     downloadName: "Rosehill-0.5",
     description:
-      "I can't live with the idea I might never see him again; this is a death mission, but I'm willing. Enter the school haunted by the past dodge anything that wants to rip your soul out of your body. Because what's on the other side is much... much more horrifying.",
+      "I can't live with the idea I might never see him again. this is a death mission, but I'm willing. Enter the school haunted by the past dodge anything that wants to rip your soul out of your body. Because what's on the other side is much... much more horrifying.",
     image: {
-      icon: "https://i.imgur.com/8Q9QY0l.png",
+      icon: "https://i.imgur.com/StqlwxX.png",
     },
     tags: ["development", "horror"],
     verified: true,
   },
   {
-    name: "Project L",
+    name: "Comming soon",
     downloadName: "Project_L-0.1",
     description: "Currently in development. Stay tuned!",
     image: {
       icon: "https://i.imgur.com/8Q9QY0l.png",
     },
     tags: ["next"],
-    verified: true,
+    verified: false,
   },
 ];
 
