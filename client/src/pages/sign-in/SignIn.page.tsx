828107b96cb3fc6d7cd0c975ee9f578f2df99050
Game Glide <sinha.soham@outlook.com>
2024-03-27 20:25:50 +0530

    Create 1/2 of sign in page

--- client/src/pages/sign-in/SignIn.page.tsx
+++ client/src/pages/sign-in/SignIn.page.tsx
@@ -1,10 +1,14 @@
 import { observer } from "mobx-react";
 import "./signIn.styles.css";
 import { useFormik } from "formik";
-import { Input } from "@nextui-org/react";
+import SelectInput from "@/components/selectInput/SelectInput.component";
 import { signInSchema } from "@/schemas/signInSchema";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+import { useState } from "react";
+import { Button } from "@nextui-org/react";
 
 const SignInForm = () => {
+  const [isPasswordVisibility, setIsPasswordVisibility] = useState(false);
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -14,9 +18,48 @@ const SignInForm = () => {
     validationSchema: signInSchema,
   });
 
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisibility(!isPasswordVisibility);
+
   return (
     <form autoComplete="off">
-      <Input type="email" label="email" />
+      <SelectInput
+        type="email"
+        label="email"
+        className="form-input"
+        size="md"
+        radius="lg"
+        variant="flat"
+      />
+      <SelectInput
+        label="password"
+        className="form-input"
+        size="md"
+        radius="lg"
+        variant="flat"
+        endContent={
+          <button
+            className="focus:outline-none"
+            type="button"
+            onClick={togglePasswordVisibility}
+          >
+            {isPasswordVisibility ? (
+              <FaEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+            ) : (
+              <FaEye className="text-2xl text-default-400 pointer-events-none" />
+            )}
+          </button>
+        }
+        type={isPasswordVisibility ? "text" : "password"}
+      />
+      <div className="submit-btn-row">
+        <Button color="primary" variant="flat" type="submit">
+          Continue
+        </Button>
+        <Button color="primary" variant="shadow">
+          Sign up
+        </Button>
+      </div>
     </form>
   );
 };
