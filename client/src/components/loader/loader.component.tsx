d12d3ac99b5586b39eb9262c793cd72c57fac4d0
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2024-04-04 00:09:22 +0100

    random loading wheel messages
    also corrected the package

--- client/src/components/loader/loader.component.tsx
+++ client/src/components/loader/loader.component.tsx
@@ -1,5 +1,24 @@
 import { Spinner } from "@nextui-org/react";
 
+const loadingMessages = [
+  "Cookin'",
+  "Getting ready",
+  "Lookin' good",
+  "Getting ready",
+  "One moment",
+  "Working on it",
+  "Look behind you",
+  "Fixing all the error messages",
+  "Loading times are a feature",
+  "Loading all the love from Select",
+  "(╯°□°）╯︵ ┻━┻",
+];
+
+const getRandomMessage = () => {
+  const randomIndex = Math.floor(Math.random() * loadingMessages.length);
+  return loadingMessages[randomIndex];
+};
+
 interface LoaderProps {
   msg?: string;
 }
@@ -10,11 +29,13 @@ export interface LoadingState {
 }
 
 const Loader: React.FC<LoaderProps> = ({ msg }) => {
+  const randomMessage = msg || getRandomMessage();
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="grid justify-center">
         <Spinner size="lg" />
-        <p className="font-bold mt-5">Working on it</p>
+        <p className="font-bold mt-5">{randomMessage}</p>
       </div>
     </div>
   );
