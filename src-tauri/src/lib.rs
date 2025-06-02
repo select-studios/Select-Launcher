89d93913b7816933f9a292ee53004a9d0af7af70
Game Glide <sinha.soham@outlook.com>
2025-06-02 12:11:54 +0530

    added tauri devtools console

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,8 +6,12 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[cfg(debug_assertions)]
+    let dev_tools = tauri_plugin_devtools::init();
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(dev_tools)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
